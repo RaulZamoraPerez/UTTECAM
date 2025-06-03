6157142ecea593f188781f18e9ce4465bafc8a11
DavidHdzMdz <138507325+JDavid2809@users.noreply.github.com>
2025-06-03 13:24:28 -0600

    vinculacion

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -9,12 +9,9 @@ export const AppLayout = () => {
         <>
             <Navbar/>
 
-
             <Outlet/>
 
             <Pleca/>
-
-
 
                {/* Contenedor global para los toasts */}
       <ToastContainer  />
