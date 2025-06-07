22523513fe9b65249565cbf2fd636589e4a753fb
Raul zamora <raulzamoraperez440@gmail.com>
2025-06-06 21:31:40 -0600

    organigrama, vistas pdf

--- src/data/Organigrama.ts
+++ src/data/Organigrama.ts
@@ -1,5 +1,3 @@
-import type { OrganizationChartNodeData } from "primereact/organizationchart";
-
 
 
 export interface CustomOrgNode {
@@ -21,8 +19,8 @@ const dataOrganigrama : CustomOrgNode[]= [
     expanded: true,
     type: "person",
     data: {
-      image: "https://img.freepik.com/foto-gratis/hombre-tiro-medio-corrigiendo-errores-gramaticales_23-2150171419.jpg?semt=ais_items_boosted&w=74",
-      name: "Dr. Raúl Rector",
+      image: "public/Organigrama/Rector.png",
+      name: "Ing. Enrique Salvador Fernández Lozada ",
       title: "Rector",
     },
     children: [
@@ -30,151 +28,168 @@ const dataOrganigrama : CustomOrgNode[]= [
         expanded: true,
         type: "person",
         data: {
-          image: "",
-          name: "Mtra. Ana Lira",
-          title: "Directora Académica",
+          image: "public/Organigrama/AbogadoGeneral.png",
+          name: "Mtro. Eleazar Carrillo Camacho ",
+          title: "ENCARGADO DEL ÁREA DE ABOGADO GENERAL"
         },
         children: [
           
-          {
-            expanded: true,
-            type: "person",
-            data: {
-              name: "Dra. Lucía Navarro",
-              title: "Jefa División Químico Biológicas",
-              image: "",
-            },
-            children: [
-              {
-                type: "person",
-                data: {
-                  name: "Ing. Carlos Mendoza",
-                  title: "Coordinador de Química",
-                  image: "",
-                },
-              },
-              {
-                type: "person",
-                data: {
-                  name: "Lic. Ana Pérez",
-                  title: "Coordinadora de Biología",
-                  image: "",
-                },
-              },
-            ],
-          },
-          {
-            expanded: true,
-            type: "person",
-            data: {
-              name: "Mtro. Esteban Gómez",
-              title: "Jefe División TIC",
-              image: "",
-            },
-            children: [
-              {
-                type: "person",
-                data: {
-                  name: "Ing. Patricia Ruiz",
-                  title: "Coordinadora de Sistemas",
-                  image: "",
-                },
-              },
-              {
-                type: "person",
-                data: {
-                  name: "Ing. Luis Hernández",
-                  title: "Coordinador de Redes",
-                  image: "",
-                },
-              },
-            ],
-          },
-          {
-            expanded: true,
-            type: "person",
-            data: {
-              name: "Lic. Karen López",
-              title: "Jefa de Negocios",
-              image: "",
-            },
-            children: [
-        
-              
-              {
-                type: "person",
-                data: {
-                  name: "Lic. Gabriela Soto",
-                  title: "Coordinadora de Finanzas",
-                  image: "",
-                },
-              },
-              
-              {
-                type: "person",
-                data: {
-                    name: "Lic. Mariana Torres",
-                    title: "Coordinadora de Administración",
-                    image: "",
-                    
-                    },
-                
-              },
-            ],
-          },
+          // {
+          //   expanded: true,
+          //   type: "person",
+          //   data: {
+          //     name: "Dra. Lucía Navarro",
+          //     title: "Jefa División Químico Biológicas",
+          //     image: "",
+          //   },
+          //   children: [
+          //     {
+          //       type: "person",
+          //       data: {
+          //         name: "Ing. Carlos Mendoza",
+          //         title: "Coordinador de Química",
+          //         image: "",
+          //       },
+          //     },
+          //     {
+          //       type: "person",
+          //       data: {
+          //         name: "Lic. Ana Pérez",
+          //         title: "Coordinadora de Biología",
+          //         image: "",
+          //       },
+          //     },
+          //   ],
+          // },
+          // {
+          //   expanded: true,
+          //   type: "person",
+          //   data: {
+          //     name: "Mtro. Esteban Gómez",
+          //     title: "Jefe División TIC",
+          //     image: "",
+          //   },
+          //   children: [
+          //     {
+          //       type: "person",
+          //       data: {
+          //         name: "Ing. Patricia Ruiz",
+          //         title: "Coordinadora de Sistemas",
+          //         image: "",
+          //       },
+          //     },
+          //     {
+          //       type: "person",
+          //       data: {
+          //         name: "Ing. Luis Hernández",
+          //         title: "Coordinador de Redes",
+          //         image: "",
+          //       },
+          //     },
+          //   ],
+          // },
+         
         ],
       },
       {
         expanded: true,
         type: "person",
         data: {
-          image: "",
-          name: "Lic. Juan Méndez",
-          title: "Director Administrativo",
+          image: "public/Organigrama/ContraloriaInterna.png",
+          name: "Abg. Alain Eloy Alvarez Sánchez ",
+          title: "ENCARGADO DEL ÁREA DE  CONTRALORÍA INTERNA",
         },
-        children: [
-           
-           {
-                type: "person",
-                data: {
-                  name: "Lic. Andrés Torres",
-                  title: "Coordinador de Mercadotecnia",
-                  image: "",
-                },
-              },
-          {
-            expanded: true,
-            type: "person",
-            data: {
-              name: "Lic. Diana Vargas",
-              title: "Jefa de Vinculación",
-              image: "",
-            },
-            children: [
+       
+        
+      },{
+      expanded: true,
+        type: "person",
+        data: {
+          image: "public/Organigrama/secretariaAcademica.png",
+          name: " Mtro. Carlos Islas Contreras ",
+          title: "ENCARGADO DE LA SECRETARIA ACADÉMICA ",
+        },
+        children:[
+            
                {
                 type: "person",
                 data: {
-                  name: "Lic. xdxd",
-                  title: "Coordinador de Mercadotecnia",
-                  image: "",
-                  
+                  image: "public/Organigrama/directores/Agricultura.png",
+                  name: " lng. Laura Rodríguez Peláez ",
+                  title: " DIRECCIÓN  DEL P.E.  AGRICULTURA SUSTENTBLE Y  PROTEGIDA ",
+  
                 },
-                  children : [
-                    {
-                        type: "person",
-                        data: {
-                            name: "Lic. Raúl Pérez",
-                            title: "Coordinador de Contabilidad",
-                            image: "",
-                        },
-                    }
-                  ]
-                ,
               },
-             
+                {
+                  type: "person",
+                  data: {
+                    image: "public/Organigrama/directores/conta.png",
+                    name: "Mtro. Luis Enrique Manzano Martínez ",
+                    title: "ENC. DE LA  DIRECCIÓN  DEL P.E. CONTADURÍA  ",
+                    
+                  },
+                },
+                
+                {
+                  type: "person",
+                  data: {
+                    image: "public/Organigrama/directores/admin.png",
+                    name: "Mtra. Miriam Garcilazo Alcántara  ",
+                    title: "DIRECTORA  DEL P.E.ADMINISTRACIÓN   ",
+                    
+                  },
+                },
+                
+                {
+                  type: "person",
+                  data: {
+                    image: "public/Organigrama/directores/mercado.png",
+                    name: "Mtro. Jesús Guadalupe Jiménez de Rosas   ",
+                    title: "DIRECCIÓN DEL P.E. INNOVACIÓN DE NEGOCIOS Y MERCADOTECNIA ",
+                    
+                  },
+                },
+                {
+                  type: "person",
+                  data: {
+                    image: "public/Organigrama/directores/industrial.png",
+                    name: "Ing. Job Armando Henández Cortés",
+                    title: " DIRECCIÓN  DEL P.E.INGENIERÍA INDUSTRIAL ",
+                    
+                  },
+                  children:[
+                    {
+                      type: "person",
+                      data: {
+                        image: "public/Organigrama/directores/mant_indus.png",
+                        name: "Ing. José Mario Cepeda Sorcia",
+                        title: "DIRECCIÓN  DEL P.E. MANTENIMIENTO  INDUSTRIA ",
+                        
+                      },
+                    },
+                  ]
+                },
+                {
+                  type: "person",
+                  data: {
+                    image: "public/Organigrama/directores/industrial.png",
+                    name: "Ing. Job Armando Henández Cortés",
+                    title: " DIRECCIÓN  DEL P.E.INGENIERÍA INDUSTRIAL ",
+                    
+                  },
+                },
+              
             ],
-          },
-        ],
+        
+      },
+      {
+      expanded: true,
+        type: "person",
+        data: {
+          image: "public/Organigrama/secretarioVinculacion.png",
+          name: "  Lic. Daniel Huerta Conde ",
+          title: "SECRETARIO  DE VINCULACIÓN ",
+        },
       },
     ],
   },
--- src/views/Nosotros/PDF.tsx
+++ src/views/Nosotros/PDF.tsx
@@ -1,20 +1,26 @@
-import { useParams } from "react-router-dom";
+import {  useParams} from "react-router-dom";
 import { useState } from "react";
 import { Spinner } from "@/components/Spinner";
 import { ContenedorPDF } from "@/components/Pdf/ContenedorPDF";
 
 
-const PDF = () => {
-  const { title } = useParams();
-  const [isLoading, setIsLoading] = useState(true);
 
-  const fakePDFUrl = "/becas.pdf";
+
+const PDF = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+const { title } = useParams(); 
+
+// const fakePDFUrl2 = `/ProductosInvestigacion/ProductosInves_2018/Modelo de control interno admnistrativo como estrategia para miempresas.pdf`;
+const fakePDFUrl2 = `/ProductosInvestigacion/ProductosInves_2018/${title?.trim()}`;
+
+;
 
   return (
     <div className="h-screen w-screen flex flex-col">
       <div className="p-4 bg-white mt-10">
         <h1 className="text-2xl font-light text-gray-700 text-center">
-          {title}
+          {/* {title} */}
         </h1>
       </div>
 
@@ -22,7 +28,7 @@ const PDF = () => {
       {isLoading && <Spinner text=" cargando documento... " />}
 
       <ContenedorPDF
-        fakePDFUrl={fakePDFUrl}
+        fakePDFUrl={fakePDFUrl2}
         isLoading={isLoading}
         setIsLoading={setIsLoading}
       />
