5448dfcaad687e293a5e3374a1f0ffd8635a2312
Lisa2900 <lisandroflores23027@gmail.com>
2025-08-14 12:42:30 -0600

    Agregar submenús para 'Trámites' y 'Convocatoria a trámite de título profesional' en 'Servicios escolares' del Navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -74,7 +74,29 @@ export default function Navbar() {
       href: "/admision",
       submenu: [
         { label: "Proceso de admisión", href: "/proceso-admision" },
-        { label: "Trámites", href: "/serviciosEscolares" },
+        {
+          label: "Trámites",
+          href: "/serviciosEscolares",
+          submenu: [
+            {
+              label: "Reinscripción a Ingeniería/Licenciatura (7º cuatrimestre)",
+              href: "/reinscripcion-ingenieria-licenciatura"
+            }
+          ]
+        },
+        {
+          label: "Convocatoria a trámite de título profesional",
+          submenu: [
+            {
+              label: "CONVOCATORIA A TRÁMITE DE TÍTULO PROFESIONAL. Egresados de 11º cuatrimestre",
+              href: "/Convocatoria%20a%20tr%C3%A1mite%20de%20t%C3%ADtulo%20profesional/2%20Convocatoria%20LIC%202025%2011o%20cuatrimestre.pdf",
+            },
+            {
+              label: "CONVOCATORIA A TRÁMITE DE TÍTULO PROFESIONAL. Egresados de 6º cuatrimestre",
+              href: "/Convocatoria%20a%20tr%C3%A1mite%20de%20t%C3%ADtulo%20profesional/Convocatoria%20TSU%202025%206o%20cuatrimestre.pdf",
+            },
+          ],
+        },
         {
           label: "Requisitos",
           href: "/requisitos",
